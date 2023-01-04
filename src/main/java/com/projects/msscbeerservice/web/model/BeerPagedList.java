6802e00f7dba06b1f54202161da6978829d376e2
lesterbek <lesterbek@gmail.com>
2023-01-05 00:25:47 +0800

    3rd commit

---
+++ src/main/java/com/projects/msscbeerservice/web/model/BeerPagedList.java
@@ -0,0 +1,20 @@
+package com.projects.msscbeerservice.web.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import java.io.Serializable;
+import java.util.List;
+
+public class BeerPagedList extends PageImpl<BeerDto>{
+    public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPagedList(List<BeerDto> content) {
+        super(content);
+    }
+}
